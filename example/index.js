9efd1218393803395dd8c27989bd8b37534f5e9a
zeroice <nucleardreamer@gmail.com>
2019-03-04 13:38:24 -0800

    updating examples

--- example/index.js
+++ example/index.js
@@ -2,6 +2,8 @@ const { join } = require('path')
 const _ = require('lodash')
 const QuickgRpc = require('quick-grpc')
 
+let startPort = 9000
+
 async function start () {
   let client = await new QuickgRpc({
     host: process.env.HOST || '0.0.0.0:9101',
@@ -9,7 +11,25 @@ async function start () {
   })
   let webcam = await client.webcam()
   webcam.info(undefined, function (err, { webcams }) {
-    console.log('* WEBCAMS:', JSON.stringify(webcams, null, 2))
+    if (err || !webcams.length) throw err || (new Error('No webcams! Exiting'))
+    let cam = webcams[0]
+    let webcamId = cam.webcamId
+      let StreamRequest = {
+        webcamId,
+        mode: _.reverse(cam.availableModes)[0],
+        port: startPort
+      }
+      // start stream of first webcam found
+      webcam.startStream(StreamRequest, function (err, { url }) {
+        if (err) throw err
+        console.log(`Webcam /dev/${webcamId}: ${url}`)
+        // stop the stream after 10 seconds
+        setTimeout(function () {
+          webcam.stopStream(StreamRequest, function () {
+            process.exit(0)
+          })
+        }, 10000)
+      })
   })
 }
 
